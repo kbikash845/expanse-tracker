406b7fa80049f9fa1be905f3994530217434e39a
Bikash kumar <kbkash845@gmail.com>
2023-08-08 23:32:26 +0530

    complete Profile

---
+++ src/components/Layout/MainNavigation.js
@@ -0,0 +1,44 @@
+import { Link } from 'react-router-dom';
+
+import classes from './MainNavigation.module.css';
+import { useContext } from 'react';
+import AuthContext from '../Store/Auth-Context';
+
+const MainNavigation = () => {
+  const authCtx=useContext(AuthContext)
+
+  const isloggedin=authCtx.isLonggedIn
+
+  const longoutHandler=()=>{
+    authCtx.lonout();
+  }
+  return (
+    <header className={classes.header}>
+      <Link to='/'>
+        <div className={classes.logo}>Expanse Tracker</div>
+      </Link>
+      <nav>
+        <ul>
+          {!isloggedin && (
+            <li>
+            <Link to='/auth'>Login</Link>
+          </li>
+          )}
+         {isloggedin && (
+           <li>
+           <Link to='/profile'>Profile</Link>
+         </li>
+          )}
+          {isloggedin && (
+             <li>
+             <button onClick={longoutHandler}>Logout</button>
+           </li>
+          )}
+         
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default MainNavigation;
