d8de23677bba76720d1d576dd2f57c1a544ed23f
Bikash kumar <kbkash845@gmail.com>
2023-08-11 22:55:55 +0530

    some chhanged in css

--- src/components/StartingPage/ExpanseSubmitDetails.js
+++ src/components/StartingPage/ExpanseSubmitDetails.js
@@ -7,24 +7,31 @@ function ExpanseSubmitDetails({expenseData,onDelete, onEdit}) {
     
     return (
         <div className="details-container">
-      {/* <h2>Expense Details</h2> */}
+      {/*  */}
 
       <ul className="detail-list">
-        <li>
-          <span className="detail-label">ExpanseName:</span>
-          <span className="detail-value">{expenseData.title} ,</span>
+        <li className='amount-date'>
+          
+        <span className="detail-value"> ${expenseData.amount} </span>
+          <span className="detail-value">
+          {new Date(expenseData.date).toLocaleDateString('en-US', {
+              year: 'numeric',
+              month: 'long',
+              day: 'numeric',
+            })}
+             </span>
+          </li>
+          <li className='title-delete'>
+            <div>
+             <span className="detail-value">{expenseData.title} </span>
+             </div>
+             <div style={{display:"flex",gap:"1.8rem"}}>
+             <span><button onClick={onEdit}  className="edit">Edit</button></span>
+          <span ><button onClick={onDelete} className="delete">Delete</button></span>
         
-        
-          <span className="detail-label">Amount:-</span>
-          <span className="detail-value"> ${expenseData.amount} ,</span>
-        
-        
-          <span className="detail-label">Description:-</span>
-          <span className="detail-value">{expenseData.description} </span>
-        
-          <span className="detail-value"><button onClick={onEdit}>Edit</button></span>
-          <span className="detail-value"><button onClick={onDelete}>Delete</button></span>
-        </li>
+          </div>
+         
+           </li>
       </ul>
     </div>
   );
